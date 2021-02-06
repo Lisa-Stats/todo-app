9aa953656b465c0f44218750347f4388e8cf8277
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-02-05 20:25:08 -0500

    Create response functions and insert-user interceptor

--- src/todo_app/interceptors.clj
+++ src/todo_app/interceptors.clj
@@ -1,7 +1,8 @@
 (ns todo-app.interceptors
   (:require [clojure.java.io :as io]
             [integrant.core :as ig]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql]))
 
 (defonce db-url
   (-> "config.edn"
@@ -16,3 +17,27 @@
    :enter (fn [context]
             (let [db-conn (jdbc/with-options db-url {})]
               (assoc context :db-conn db-conn)))})
+
+(defn response [status body]
+  {:status status :body body})
+
+(def ok       (partial response 200))
+(def created  (partial response 201))
+(def accepted (partial response 202))
+
+(defn make-user [username pw email]
+  {:username username
+   :password pw
+   :email email})
+
+(def insert-user
+  {:name ::insert-user
+   :enter
+   (fn [context]
+     (let [username (-> context :request :json-params :username)
+           pw       (-> context :request :json-params :password)
+           email    (-> context :request :json-params :email)
+           new-user (make-user username pw email)]
+       (sql/insert! db-url :users new-user)
+       (assoc context
+              :response (created new-user))))})
